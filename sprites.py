d69e2a547ac36af1d1e591aa07c0a5398987e498
KevinUstar <62357697+KevinUstar@users.noreply.github.com>
2020-04-30 15:02:58 -0700

    prints game over if you touch wall

--- sprites.py
+++ sprites.py
@@ -11,7 +11,7 @@ class Player(pg.sprite.Sprite):
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.vx, self.vy = 0, 0
-        self.x = x * TILESIZE
+        self.x = x * TILESIZE 
         self.y = y * TILESIZE
         # self.rect.x = self.x
         # self.rect.y = self.y
@@ -19,36 +19,41 @@ class Player(pg.sprite.Sprite):
         self.vx, self.vy = 0, 0
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
-            self.vx = -PLAYER_SPEED
+            self.vx += -PLAYER_SPEED
         elif keys[pg.K_d]:
-            self.vx = PLAYER_SPEED
+            self.vx += PLAYER_SPEED
         elif keys[pg.K_w]:
-            self.vy = -PLAYER_SPEED
+            self.vy += -PLAYER_SPEED
         elif keys[pg.K_s]:
-            self.vy = PLAYER_SPEED
+            self.vy += PLAYER_SPEED
         # elif self.vx != 0 and self.vy != 0:
         #     self.vx *= 0.7071
         #     self.vy *= 0.7071
-
-    def collide_with_walls(self, dir):
-        if dir == 'x':
-            hits = pg.sprite.spritecollide(self, self.game.walls, False)
-            if hits:
-                if self.vx > 0:
-                    self.x = hits[0].rect.left - self.rect.width
-                if self.vx < 0:
-                    self.x = hits[0].rect.right
-                self.vx = 0
-                self.rect.x = self.x
-        if dir == 'y':
-            hits = pg.sprite.spritecollide(self, self.game.walls, False)
-            if hits:
-                if self.vy > 0:
-                    self.y = hits[0].rect.top - self.rect.height
-                if self.vy < 0:
-                    self.y = hits[0].rect.bottom
-                self.vy = 0
-                self.rect.y = self.y
+    def collision(self):
+        hit = pg.sprite.spritecollide(self, self.game.walls, False)
+        if hit:
+            print("game over")
+            self.vx = 0
+            self.vy = 0
+    # def collide_with_walls(self, dir):
+    #     if dir == 'x':
+    #         hits = pg.sprite.spritecollide(self, self.game.walls, False)
+    #         if hits:
+    #             if self.vx > 0:
+    #                 self.x = hits[0].rect.left - self.rect.width
+    #             if self.vx < 0:
+    #                 self.x = hits[0].rect.right
+    #             self.vx = 0
+    #             self.rect.x = self.x
+    #     if dir == 'y':
+    #         hits = pg.sprite.spritecollide(self, self.game.walls, False)
+    #         if hits:
+    #             if self.vy > 0:
+    #                 self.y = hits[0].rect.top - self.rect.height
+    #             if self.vy < 0:
+    #                 self.y = hits[0].rect.bottom
+    #             self.vy = 0
+    #             self.rect.y = self.y
                 
 
     def update(self):
@@ -56,9 +61,11 @@ class Player(pg.sprite.Sprite):
         self.x += self.vx * self.game.dt
         self.y += self.vy * self.game.dt
         self.rect.x = self.x
-        self.collide_with_walls('x')
+        self.collision()
+        # self.collide_with_walls('x')
         self.rect.y = self.y
-        self.collide_with_walls('y')
+        # self.collide_with_walls('y')
+        
         
 
 # class Body(pg.sprite.Sprite):
